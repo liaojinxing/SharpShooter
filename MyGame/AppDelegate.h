8bfbc70aaa4a3a8f6dc9704b848e29cfa8b2b8f3
liaojinxing <jinxingliao@gmail.com>
2014-04-08 11:11:16 +0800

    Initial Commit

---
+++ MyGame/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MyGame
+//
+//  Created by liaojinxing on 14-4-8.
+//  Copyright (c) 2014年 jinxing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MyGame/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  MyGame
+//
+
+//  Copyright (c) 2014年 jinxing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SpriteKit/SpriteKit.h>
+
+@interface ViewController : UIViewController
+
+@end
